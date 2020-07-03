95993341b9efe1f63e652502c2b183d2752b4430
Michael Behrisch <michael.behrisch@dlr.de>
2020-07-03 22:03:42 +0200

    enforcing python3

--- make-and-sim.py
+++ make-and-sim.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (C) 2019-2020 German Aerospace Center (DLR) and others.
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License 2.0 which is available at
